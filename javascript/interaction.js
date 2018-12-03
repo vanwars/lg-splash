ab1d3fc3eab03aa28dc211913a0a2dad583d8319
Ellen Van Wyk <>
2018-12-03 08:20:34 -0800

    layout and assets

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -1,13 +1,9 @@
-$(document).ready(function(){
-  $('#nav').localScroll(800);
-  
-  //.parallax(xPosition, speedFactor, outerHeight) options:
-  //xPosition - Horizontal position of the element
-  //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-  //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-  $('#intro').parallax("50%", 0.1);
-  $('#second').parallax("50%", 0.1);
-  $('.bg').parallax("50%", 0.4);
-  $('#third').parallax("50%", 0.3);
 
-})
+
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".navbar");
+//     $nav.toggleClass('navbar-light bg-light navbar-dark', $(this).scrollTop() > $nav.height());
+//     $nav.toggleClass('navbar-light bg-light navbar-dark', $(this).scrollTop() > $nav.height());
+//   });
+// });
